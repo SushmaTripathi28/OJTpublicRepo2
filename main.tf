61a873a7ece7aa770b7a2404a9d5e5312b6786f8
SushmaTripathi28 <128452108+SushmaTripathi28@users.noreply.github.com>
2024-06-12 14:11:22 +0530

    Create main.tf for dashbords

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "google_monitoring_dashboard" "dashboard" {
+  dashboard_json = <<EOF
+  {
+  "displayName": "New Dashboard -DemoOJT",
+  "dashboardFilters": [],
+  "mosaicLayout": {
+    "columns": 48,
+    "tiles": [
+      {
+        "widget": {
+          "title": "VM Instance - CPU utilization [MEAN]",
+          "xyChart": {
+            "chartOptions": {
+              "mode": "COLOR"
+            },
+            "dataSets": [
+              {
+                "minAlignmentPeriod": "60s",
+                "plotType": "LINE",
+                "targetAxis": "Y1",
+                "timeSeriesQuery": {
+                  "timeSeriesFilter": {
+                    "aggregation": {
+                      "alignmentPeriod": "60s",
+                      "perSeriesAligner": "ALIGN_MEAN"
+                    },
+                    "filter": "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" resource.type=\"gce_instance\""
+                  }
+                }
+              }
+            ],
+            "thresholds": [],
+            "yAxis": {
+              "label": "",
+              "scale": "LINEAR"
+            }
+          }
+        },
+        "height": 16,
+        "width": 24
+      }
+    ]
+  },
+  "labels": {}
+}
+EOF
+}
